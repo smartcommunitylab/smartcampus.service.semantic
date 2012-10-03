4094a16f48a7a9bc146e29bddedaaaa2186922bb
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-10-03 08:17:37 +0000

    Test

--- src/test/java/eu/trentorise/smartcampus/services/ou/test/TestDataFlow.java
+++ src/test/java/eu/trentorise/smartcampus/services/ou/test/TestDataFlow.java
@@ -1,24 +1,38 @@
 package eu.trentorise.smartcampus.services.ou.test;
 
 import it.sayservice.platform.core.common.exception.ServiceException;
+import it.sayservice.platform.servicebus.test.DataFlowTestHelper;
 
-import java.io.File;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.util.HashMap;
+import java.util.Map;
+
+import eu.trentorise.smartcampus.services.semantic.data.message.Semantic.Data;
+import eu.trentorise.smartcampus.services.semantic.impl.CreateEntityDataFlow;
 
 public class TestDataFlow {
 
 	public static void main(String[] args) throws ServiceException, ClassNotFoundException, MalformedURLException {
-		URL[] urls = new URL[]{new File("tmp").toURI().toURL()};
-		ClassLoader cl = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-		System.err.println(cl.loadClass("eu.trentorise.smartcampus.services.semantic.data.message.Semantic$Data"));
-//		DataFlowTestHelper helper = new DataFlowTestHelper();
-//		Map<String, Object> out = helper.executeDataFlow(
-//				"eu.trentorise.smartcampus.services.ou.OperaUniService", 
-//				"GetLocations", 
-//				new CreateEntityDataFlow(), 
-//				new HashMap<String, Object>());
-//		System.err.println(out);
+//		URL[] urls = new URL[]{new File("tmp").toURI().toURL()};
+//		ClassLoader cl = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
+//		System.err.println(cl.loadClass("eu.trentorise.smartcampus.services.semantic.data.message.Semantic$Data"));
+		DataFlowTestHelper helper = new DataFlowTestHelper();
+		
+		Map<String,Object> params = new HashMap<String, Object>();
+		params.put("actorId", 37L);
+		Data data = Data.newBuilder()
+		.setName("test evento Università")
+		.setType("event")
+		.build();
+		
+		
+		params.put("data", data);
+		
+		Map<String, Object> out = helper.executeDataFlow(
+				"eu.trentorise.smartcampus.services.semantic.SemanticService", 
+				"CreateEntity", 
+				new CreateEntityDataFlow(), 
+				params);
+		System.err.println(out);
 	}
 }
